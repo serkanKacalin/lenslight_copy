27cc64c33704a71c5ea50231f72a12289b09b457
Serkan Kaçalın <kacalnn53@gmail.com>
2023-05-25 20:12:26 +0300

    ejs template engine and partials

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+// ejs template engine
+app.set('view engine', 'ejs'); // ejsyi template engine olarak belirledik.
+
+// static files middleware(ara yazılım)
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
-    res.send('INDEX SAYFASI');
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(port, () => {
