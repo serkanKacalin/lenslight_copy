188ee507898f7e480cf8f7fe31e6caf70ece1e5c
Serkan Kaçalın <kacalnn53@gmail.com>
2023-07-19 16:05:48 +0300

    Authentication, Authorization, JSON WEB TOKEN

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -3,10 +3,13 @@ import * as pageController from '../controllers/pageController.js'; // * as ile 
 import * as authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
-
+// console.log('Authen... token  :: ', typeof(authMiddleware.authenticateToken))
 router
   .route('/')
-  .get(authMiddleware.authenticateToken, pageController.getIndexPage);
+//   .get(authMiddleware.authenticateToken, pageController.getIndexPage); ilk parametremiz bir fonksiyon olduğu için token null dönüyor
+// bu olay da index sayfasına girerken token bulunamadı hatasına sebep oluyor o yüzden yorum satırına aldım.
+  .get(pageController.getIndexPage);
+
 router.route('/about').get(pageController.getAboutPage);
 router.route('/register').get(pageController.getRegisterPage);
 router.route('/login').get(pageController.getLoginPage);    
